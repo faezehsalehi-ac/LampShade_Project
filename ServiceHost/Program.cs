00fa617d0e8223d92cc3386f684b299ccdf2dd58
faezehsalehi <faezehsalehi.ac@gmail.coom>
2024-04-06 13:13:17 +0330

    new edit

---
+++ ServiceHost/Program.cs
@@ -0,0 +1,132 @@
+using System.Collections.Generic;
+/*using _0_Framework.Application.Email;
+using _0_Framework.Application.Sms;
+using _0_Framework.Application.ZarinPal;*/
+using _0_Framework.Application;
+//using AccountManagement.Configuration;
+//using BlogManagement.Infrastructure.Configuration;
+//using CommentManagement.Infrastructure.Configuration;
+//using DiscountManagement.Configuration;
+//using InventoryManagement.Infrastructure.Configuration;
+//using InventoryManagement.Presentation.Api;
+//using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using ServiceHost;
+using ShopManagement.Configuration;
+//using ShopManagement.Presentation.Api;
+using System.Configuration;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+//using _0_Framework.Infrastructure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Data.Sqlite;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+//  Register Services
+builder.Services.AddHttpContextAccessor();
+var connectionString = builder.Configuration.GetConnectionString("LocalDatabase");
+
+ShopManagementBootstrapper.Configure(builder.Services, connectionString);
+/*DiscountManagementBootstrapper.Configure(builder.Services, connectionString);
+InventoryManagementBootstrapper.Configure(builder.Services, connectionString);
+BlogManagementBootstrapper.Configure(builder.Services, connectionString);
+CommentManagementBootstrapper.Configure(builder.Services, connectionString);
+AccountManagementBootstrapper.Configure(builder.Services, connectionString);
+
+builder.Services.AddSingleton(HtmlEncoder.Create(UnicodeRanges.BasicLatin, UnicodeRanges.Arabic));
+builder.Services.AddSingleton<IPasswordHasher, PasswordHasher>();
+builder.Services.AddTransient<IFileUploader, FileUploader>();
+builder.Services.AddTransient<IAuthHelper, AuthHelper>();
+builder.Services.AddTransient<IZarinPalFactory, ZarinPalFactory>();
+builder.Services.AddTransient<ISmsService, SmsService>();
+builder.Services.AddTransient<IEmailService, EmailService>();
+
+builder.Services.Configure<CookiePolicyOptions>(options =>
+{
+    options.CheckConsentNeeded = context => false;
+    options.MinimumSameSitePolicy = SameSiteMode.Lax;
+});
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme, o =>
+    {
+        o.LoginPath = new PathString("/Account");
+        o.LogoutPath = new PathString("/Account");
+        o.AccessDeniedPath = new PathString("/AccessDenied");
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminArea",
+        builder => builder.RequireRole(new List<string> { Roles.Administrator, Roles.ContentUploader }));
+
+    options.AddPolicy("Shop",
+        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+
+    options.AddPolicy("Discount",
+        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+
+    options.AddPolicy("Account",
+        builder => builder.RequireRole(new List<string> { Roles.Administrator }));
+});
+
+builder.Services.AddCors(options => options.AddPolicy("MyPolicy", builder =>
+    builder
+        .WithOrigins("https://localhost:5002")
+        .AllowAnyHeader()
+        .AllowAnyMethod()));
+
+builder.Services
+    .AddRazorPages()
+    .AddMvcOptions(options => options.Filters.Add<SecurityPageFilter>())
+    .AddRazorPagesOptions(options =>
+    {
+        options.Conventions.AuthorizeAreaFolder("Administration", "/", "AdminArea");
+        options.Conventions.AuthorizeAreaFolder("Administration", "/Shop", "Shop");
+        options.Conventions.AuthorizeAreaFolder("Administration", "/Discounts", "Discount");
+        options.Conventions.AuthorizeAreaFolder("Administration", "/Accounts", "Account");
+    })
+    .AddApplicationPart(typeof(ProductController).Assembly)
+    .AddApplicationPart(typeof(InventoryController).Assembly)
+    .AddNewtonsoftJson();
+*/
+
+var app = builder.Build();
+
+
+// Create Http Pipeline
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseAuthentication();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseCookiePolicy();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseCors("MyPolicy");
+
+app.MapRazorPages();
+app.MapControllers();
+
+app.Run();
